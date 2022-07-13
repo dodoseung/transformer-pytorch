8aeb2cb0d3401b9b5f7895bdb43451e7c8ad6d85
dodoseung <sok7819@gmail.com>
2022-07-13 14:02:21 +0900

    transformer

--- transformer.py
+++ transformer.py
@@ -10,37 +10,30 @@ import matplotlib.pyplot as plt
 # Torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Hyper parameter
+d_model_value = 512
+num_heads_value = 8
+
 class PositionalEncoding():
     def __init__(self, position, d_model):
         super(PositionalEncoding, self).__init__()
         self.pos_encoding = self.positional_encoding(position, d_model)
-        
-    def get_angle(self, position, i, d_model):
-        angle = position / torch.pow(10000, (2 * (i//2) / d_model))
-        return angle
     
     def positional_encoding(self, position, d_model):
         # Get the radians
         pos = torch.range(0, position - 1).to(device).view(-1, 1)
         i = torch.range(0, d_model - 1).to(device)
-        rads = self.get_angle(pos, i, d_model)
+        rads = pos / torch.pow(10000, (2 * (i // 2) / d_model))
 
-        # Get the sinusoidal functions
-        print(rads)
-        pe_sin = torch.sin(rads[:, 0::2])
-        pe_cos = torch.cos(rads[:, 1::2])
-        
-        # Get the positional encoding values
+        # Get the positional encoding values from sinusoidal functions
         pe = np.zeros(rads.shape)
-        pe[:, 0::2] = pe_sin
-        pe[:, 1::2] = pe_cos
+        pe[:, 0::2] = torch.sin(rads[:, 0::2])
+        pe[:, 1::2] = torch.cos(rads[:, 1::2])
         
         return pe
     
-sample_pos_encoding = PositionalEncoding(50, 128)
-plt.pcolormesh(sample_pos_encoding.pos_encoding, cmap='RdBu')
-plt.xlabel('Depth')
-plt.xlim((0, 128))
-plt.ylabel('Position')
-plt.colorbar()
-plt.savefig("NLP_01_Transformer/check.png")
+# Query size: (batch, num_heads, query_sentence_len, d_model/num_heads)
+# Key size: (batch, num_heads, key_sentence_len, d_model/num_heads)
+# Value size: (batch, num_heads, value_sentence_len, d_model/num_heads)
+# Mask size(optional): (batch, 1, 1, key_sentence_len)   
+def scaled_dot_product_attention(queries, keys, values, mask):
